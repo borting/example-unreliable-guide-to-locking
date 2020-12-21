006755d6bfd0cf1ac729eaef4b80118c864e4db7
borting <bortingchen@gmail.com>
2020-12-21 17:01:54 +0800

    If other thread holds a pointer to object --> Add reference counter
    - When a reference counter drops to 0, we can actually delete it.
    - Refs:
      https://www.kernel.org/doc/htmldocs/kernel-locking/examples-refcnt.html

--- common-example.c
+++ common-example.c
@@ -7,6 +7,7 @@
 struct object
 {
 	struct list_head list;
+	unsigned int refcnt;
 	int id;
 	char name[32];
 	int popularity;
@@ -17,6 +18,35 @@ static DEFINE_SPINLOCK(cache_lock);
 static LIST_HEAD(cache);
 static unsigned int cache_num = 0;
 #define MAX_CACHE_SIZE 10
+
+static void __object_put(struct object *obj)
+{
+	if (--obj->refcnt == 0)
+		kfree(obj);
+}
+
+static void __object_get(struct object *obj)
+{
+	obj->refcnt++;
+}
+
+void object_put(struct object *obj)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&cache_lock, flags);
+	__object_put(obj);
+	spin_unlock_irqrestore(&cache_lock, flags);
+}
+
+void object_get(struct object *obj)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&cache_lock, flags);
+	__object_get(obj);
+	spin_unlock_irqrestore(&cache_lock, flags);
+}
 
 /* Must be holding cache_lock */
 static struct object *__cache_find(int id)
@@ -38,7 +68,7 @@ static void __cache_delete(struct object *obj)
 {
 	BUG_ON(!obj);
 	list_del(&obj->list);
-	kfree(obj);
+	__object_put(obj);
 	cache_num--;
 }
 
@@ -67,6 +97,7 @@ int cache_add(int id, const char *name)
 	strlcpy(obj->name, name, sizeof(obj->name));
 	obj->id = id;
 	obj->popularity = 0;
+	obj->refcnt = 1; /* The cache holds a reference */
 
 	spin_lock_irqsave(&cache_lock, flags);
 	__cache_add(obj);
@@ -84,19 +115,16 @@ void cache_delete(int id)
 	spin_unlock_irqrestore(&cache_lock, flags);
 }
 
-int cache_find(int id, char *name)
+struct object *cache_find(int id)
 {
 	struct object *obj;
-	int ret = -ENOENT;
 	unsigned long flags;
 
 	spin_lock_irqsave(&cache_lock, flags);
 	obj = __cache_find(id);
-	if (obj) {
-		ret = 0;
-		strcpy(name, obj->name);
-	}
+	if (obj)
+		__object_get(obj);
 	spin_unlock_irqrestore(&cache_lock, flags);
 
-	return ret;
+	return obj;
 }
